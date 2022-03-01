916f675a23b33931ef7638a5276bf1bde83d4fae
giulcioffi <g.cioffi@arduino.cc>
2022-03-01 10:19:48 +0100

    Remove wrong I2C synchronization on video test

--- portentax8_som/manufacturing-test-video.sh
+++ portentax8_som/manufacturing-test-video.sh
@@ -7,19 +7,6 @@ echo ""
 echo "*************************START MANUFACTURING-TEST-VIDEO*************************"
 echo ""
 
-wait_ok () {
-   var="$(i2cget -f -y 2 0x0A 0x00 w -W 3)"
-   if [[ $var != "0x4b4f" ]]; then
-      let fail=1
-      if [ $verbose == 1 ]; then
-         echo "KO :("
-      fi
-   fi
-   if [[ $var == "0x4b4f" ]] & [ $verbose == 1 ]; then
-      echo "OK :)"
-   fi
-}
-
 if [[ $1 == "-v" ]]; then
    echo "Verbose mode"
    let verbose=1
@@ -29,17 +16,10 @@ if [ $verbose == 1 ]; then
   echo "Checking if the WiFi driver is active..."
 fi
 
-DTTADDRESS=0x0A
-
-I2CREADVIDEO=0x0D
-I2CCOMPAREVIDEO=0x0E
-
 # Send I2C command I2C_READ_VIDEO (0x0D) to tell the son to enable the frame grabber
 if [ $verbose == 1 ]; then
    echo "Sending command to enable frame grabber..."
 fi
-i2cset -f -y 2 $DTTADDRESS $I2CREADVIDEO c
-wait_ok
 
 # Send the RGB frame
 echo 0 > /sys/class/graphics/fbcon/cursor_blink
@@ -52,13 +32,6 @@ if [[ $ret != "0" ]]; then
       echo "Unable to write RGB image to framebuffer"
   fi
 fi
-
-# Send I2C command I2C_COMPARE_VIDEO (0x0D) to tell the son to compare the frame with the template
-if [ $verbose == 1 ]; then
-   echo "Sending command to check the received frame..."
-fi
-i2cset -f -y 2 $DTTADDRESS $I2CCOMPAREVIDEO c
-wait_ok
 
 echo ""
 echo "**************************END MANUFACTURING-TEST-VIDEO**************************"
